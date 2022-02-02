c358741625417b54245209cea322f36fd8ca83f8
efraimrocha <efraimrocha86@gmail.com>
2022-02-01 21:53:42 -0300

    Cria formulario de adiçoa de loja

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -8,7 +8,26 @@ class StoresController < ApplicationController
     @store = Store.find(params[:id])
   end
 
+  def new
+    @store = Store.new
+  end
+
+  def create
+    @store = Store.new(store_params)
+
+    if @store.save
+      redirect_to @store
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private 
+    def store_params
+      params.require(:store).permit(:name, :cnpj, :address, :phone)
+    end
 end
+
 
 
   
